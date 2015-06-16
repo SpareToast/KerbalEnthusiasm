90502898efa90d1bac8c1a41311652e3871c6349
ST <SpareToast@users.noreply.github.com>
2015-06-16 23:42:41 +0100

    Added builder

--- KerbalEnthusiasm/StoredEnthusiasm.cs
+++ KerbalEnthusiasm/StoredEnthusiasm.cs
@@ -7,7 +7,7 @@ using UnityEngine;
 
 namespace KerbalEnthusiasm
 {
-    class KerbalEnthusiasm
+    class StoredEnthusiasm
     {
         double BaseEnthusiasm = 1.0;
         double MinEnthusiasm = 0;
@@ -15,20 +15,38 @@ namespace KerbalEnthusiasm
         double DefaultEnthusiasm = 100;
         double SoIChange = 25;
 
-        List<EnthusiasmStatus> trackedKerbals;
-        List<VesselQuality> trackedVessels;
+        private static Dictionary<String, EnthusiasmStatus> trackedKerbals;
+        private static Dictionary<Guid, VesselQuality> trackedVessels;
 
-        
+        public void PopulateLists()
+        {
+            foreach (Vessel vessel in FlightGlobals.Vessels)
+            {
+                if (vessel.loaded)
+                {
+                    trackedVessels.Add(vessel.id, GetVesselQuality (vessel.id, vessel));
+                    foreach (ProtoCrewMember kerbal in vessel.GetVesselCrew())
+                    {
+                        trackedKerbals.Add(kerbal.name, GetEnthusiasmStatus(vessel, kerbal));
+                    }
+                }
+
+            }
+        }
+
         public void UpdatePopulation(Vessel vessel)
         {
             // cycles through each crewmember on a ship for updates
             foreach (ProtoCrewMember kerbal in vessel.GetVesselCrew())
             {
+
                 EnthusiasmStatus oldKerbalStatus = GetEnthusiasmStatus(vessel, kerbal);
-                trackedKerbals.Remove(oldKerbalStatus);
-                VesselQuality oldVessel = trackedVessels.FirstOrDefault(v => v.VesselId == oldKerbalStatus.VesselID);
-                EnthusiasmStatus newKerbalStatus = UpdateEnthusiasm(kerbal, vessel, oldKerbalStatus, oldVessel);
-                trackedKerbals.Add(newKerbalStatus);
+                trackedKerbals.Remove(oldKerbalStatus.KerbalName);
+
+                VesselQuality oldVesselQuality = GetVesselQuality(oldKerbalStatus.VesselID, vessel);
+                
+                EnthusiasmStatus newKerbalStatus = UpdateEnthusiasm(kerbal, vessel, oldKerbalStatus, oldVesselQuality);
+                trackedKerbals.Add(newKerbalStatus.KerbalName, newKerbalStatus);
             }
 
         }
@@ -36,7 +54,7 @@ namespace KerbalEnthusiasm
         // If the kerbal has no status entry, make one. Otherwise, return kerbal's status
         public EnthusiasmStatus GetEnthusiasmStatus(Vessel vessel, ProtoCrewMember kerbal)
         {
-            EnthusiasmStatus kerbalStatus = trackedKerbals.FirstOrDefault(x => x.KerbalName == kerbal.name);
+            EnthusiasmStatus kerbalStatus = trackedKerbals[kerbal.name];
             if (kerbalStatus == null)
             {
                 kerbalStatus.KerbalName = kerbal.name;
@@ -67,13 +85,16 @@ namespace KerbalEnthusiasm
                 kerbalStatus.VesselID = vessel.id;
             }
 
+            //set the Kerbal's stored ship to be the ship it is now on
+            kerbalStatus.VesselID = vessel.id;
+
             return kerbalStatus;
         }
 
         // If the vessel has no quality entry, make one. Otherwise, return vessel's quality
-        public VesselQuality GetVesselQuality(Vessel vessel)
+        public VesselQuality GetVesselQuality(Guid VesselID, Vessel vessel)
         {
-            VesselQuality vesselQuality = trackedVessels.FirstOrDefault(x => x.VesselId == vessel.id);
+            VesselQuality vesselQuality = trackedVessels[VesselID];
             if (vesselQuality == null)
             {
                 vesselQuality.VesselId = vessel.id;
@@ -82,27 +103,40 @@ namespace KerbalEnthusiasm
                 vesselQuality.PopulationSize = vessel.GetVesselCrew().Count;
                 vesselQuality.TotalSpace =  vessel.GetCrewCapacity();
                 vesselQuality.FreeSpace = vesselQuality.TotalSpace - vesselQuality.PopulationSize;
-                vesselQuality.Morale = UpdateVesselQuality(vessel);
+                vesselQuality.Morale = CalculateVesselQuality(vesselQuality);
             }
             return vesselQuality;
+        }
+
+        VesselQuality UpdateVesselQuality(Vessel vessel)
+        {
+            VesselQuality newVesselQuality = new VesselQuality;
+            newVesselQuality.VesselId = vessel.id;
+            newVesselQuality.VesselName = vessel.name;
+            newVesselQuality.LastUpdate = Planetarium.GetUniversalTime();
+            newVesselQuality.PopulationSize = vessel.GetVesselCrew().Count;
+            newVesselQuality.TotalSpace =  vessel.GetCrewCapacity();
+            newVesselQuality.FreeSpace = newVesselQuality.TotalSpace - newVesselQuality.PopulationSize;
+            newVesselQuality.Morale = CalculateVesselQuality(newVesselQuality);
+            return newVesselQuality;
         }
 
         //Returns a double between .75 and 1.25 showing current vessel morale modifier
         //Todo: Include more things than current pop, likepilots, spare seats etc.
         //Todo: Split into subfunctions 
-        double UpdateVesselQuality(Vessel vessel)
+        double CalculateVesselQuality(VesselQuality vesselQuality)
         {
-            int vPopSize = VesselPopulationSize(vessel);
-            int vMaxCap = VesselMaxCapacity(vessel);
+            int vPopSize = vesselQuality.PopulationSize;
+            int vMaxCap = vesselQuality.TotalSpace;
             int vSpace = vMaxCap * 2;
-            double qMin = 0.75;
-            double qMax = 1.25;
+            double qMin = 0.67;
+            double qMax = 1.34;
             //            int badasses = VesselBaddasses();
             //            int pilots = VesselPilots();
             int company = (vPopSize - 1) / (2 * vPopSize);
-            // double legRoom = vSpace - vPopSize / 2 * vSpace;
+            double legRoom = (vSpace - vPopSize + 2) / 2 * (vSpace - vPopSize + 1);
             // double Leadership = badasses + pilots;
-            double quality = Math.Min((qMin + company), qMax);
+            double quality = Math.Min((qMin + company + legRoom), qMax);
             return quality;
         }
     }
